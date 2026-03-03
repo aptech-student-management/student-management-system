4be2a25a666409d12483764080d1935195622dfd
Vu Xuan Thien <Vuxuanthien74@gmail.com>
2026-03-03 16:39:22 +0700

    code mới

--- backend/student_management/src/main/java/com/example/student_management/repository/DepartmentRepository.java
+++ backend/student_management/src/main/java/com/example/student_management/repository/DepartmentRepository.java
@@ -1,6 +1,5 @@
 package com.example.student_management.repository;
 
-import com.example.student_management.entity.DepartmentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepartmentRepository extends JpaRepository<DepartmentEntity, String> {
