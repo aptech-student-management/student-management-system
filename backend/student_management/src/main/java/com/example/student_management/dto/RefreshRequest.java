db89b3e91859902a847bae5e812d8a8687820d61
Trieu Do Tien <trieu.tombi@gmail.com>
2026-03-06 19:04:20 +0700

    update

--- backend/student_management/src/main/java/com/example/student_management/dto/RefreshRequest.java
+++ backend/student_management/src/main/java/com/example/student_management/dto/RefreshRequest.java
@@ -1,10 +1,12 @@
 package com.example.student_management.entity;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Data
 public class RefreshRequest {
 
     private String refreshToken;
